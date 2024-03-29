8ed748caa115add7d26d6bfae3d23113e605d6ba
valgeorg97 <valentingg1997@gmail.com>
2024-03-29 16:18:14 +0200

    Add MongoDB and mongoose to NodeJS

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5000;
+
+export const mongoDBURL = "mongodb+srv://valentingg1997:7EjvfoC4jTbV4wzp@to-do-list.ioi3439.mongodb.net/tasks-collection?retryWrites=true&w=majority&appName=To-Do-List";
--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,14 @@
 import express from "express";
-import { PORT } from "./config.js";
 
 const app = express();
+
+//middleware for parsing data
+
+app.use(express.json());
 
 app.get("/", (req, res) =>{
     console.log(req);
     return res.status(200).send("test");
 });
 
-app.listen(PORT, () =>{
-    console.log(`Port: ${PORT}`)
-})
+
