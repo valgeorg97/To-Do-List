2c75840ac28faa65f685f08e329255d546d6224e
valgeorg97 <valentingg1997@gmail.com>
2024-03-30 12:16:51 +0200

    Add routes with React Router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { Routes, Route } from "react-router-dom";
+import Home from './components/Home';
+import CreateTask from './components/CreateTask';
+import TaskDetails from './components/TaskDetail';
+import EditTask from './components/EditTask';
+import DeleteTask from './components/DeleteTask';
+
 
 const App = () => {
   return (
-    <div className="bg-red-400 text-white">
-      App
-    </div>
+    <Routes> 
+      <Route path='/' element={ <Home />} />
+      <Route path='/tasks/create' element={<CreateTask />} />
+      <Route path='/tasks/details/:id' element={<TaskDetails />} />
+      <Route path='/tasks/edit/:id' element={<EditTask />} />
+      <Route path='/tasks/delete/:id' element={< DeleteTask />} />
+    </Routes>
   )
 }
 
