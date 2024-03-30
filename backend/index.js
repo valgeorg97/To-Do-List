bce0d601821231d73c3c3fff0adf9bbe1f6089de
valgeorg97 <valentingg1997@gmail.com>
2024-03-30 11:18:07 +0200

    Add CORS policy

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,18 @@
 import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
-import tasksRoute from "./routes/tasksRoute.js"
+import tasksRoute from "./routes/tasksRoute.js";
+import cors from "cors";
 
 const app = express();
+
+app.use(
+    cors({
+        origin: "http://localhost:5000",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-type"],
+    })
+)
 
 app.use(express.json());
 
