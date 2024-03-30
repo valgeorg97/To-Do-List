9beb7bafcefa527bbcd4fe7fb103eaea56e7cadf
valgeorg97 <valentingg1997@gmail.com>
2024-03-30 11:08:51 +0200

    Implement Task Model with mongoose

---
+++ backend/models/taskModel.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const taskSchema = mongoose.Schema(
+    {
+        title:{
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        status: {
+            type: String,
+            enum: ['pending', 'completed'],
+            default: 'Pending'
+        }
+    },
+    {
+      timestamps: true,
+    }
+);
+
+export const Task = mongoose.model('Task', taskSchema);
