dd35273b376baf3c19472b263e75e2959e62c9bd
valgeorg97 <valentingg1997@gmail.com>
2024-04-01 10:23:08 +0300

    Add React-Spinner

--- frontend/src/components/Spinner.jsx
+++ frontend/src/components/Spinner.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import PulseLoader from "react-spinners/PulseLoader";
 
 const Spinner = () => {
   return (
-    <div className='animate-ping w-16 h-16 m-8 rounded-full bg-sky-600'></div>
+    <div className='flex justify-center w-16 h-16 m-8 rounded-full'>
+      <PulseLoader/>
+    </div>
   )
 }
 
-export default Spinner
+export default Spinner;
