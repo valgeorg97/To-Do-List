091d7935d3e086b90fbe2eecb3d906294eaf8f63
valgeorg97 <valentingg1997@gmail.com>
2024-04-01 10:05:22 +0300

    Add style to Home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Spinner from '../components/Spinner';
 import { Link } from 'react-router-dom';
 import { AiOutlineEdit } from 'react-icons/ai';
 import { BsInfoCircle } from 'react-icons/bs';
 import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md';
+import { useParams } from 'react-router-dom';
 
 const Home = () => {
   const [tasks, setTasks] = useState([]);
   const [loading, setLoading] = useState(false);
+  const {id} = useParams();
 
   useEffect(() => {
     setLoading(true);
@@ -25,43 +26,72 @@ const Home = () => {
       });
   }, []);
 
+  const handleCheckboxChange = async (taskId) => {
+
+    const taskToUpdate = tasks.find(task => task._id === taskId);
+    if (!taskToUpdate) {
+      console.error('Task not found');
+      return;
+    }
+    
+    const updatedTask = { ...taskToUpdate, status: taskToUpdate.status === 'completed' ? 'pending' : 'completed' };
+  
+    try {
+      setLoading(true);
+      await axios.put(`http://localhost:5000/tasks/${taskId}`, updatedTask);
+      setLoading(false);
+      setTasks(prevTasks => prevTasks.map(task => task._id === taskId ? updatedTask : task));
+    } catch (error) {
+      setLoading(false);
+      console.error('Error occurred while updating task status:', error);
+    }
+  };
+
   return (
-    <div className="flex justify-center items-center h-full">
-    <div className="p-4 w-2/5">
-      <div className='flex justify-between items-center'>
-        <h1 className='text-3xl my-8'>Tasks</h1>
-        <Link to='/tasks/create'>
-          <MdOutlineAddBox className='text-sky-800 text-4xl hover:scale-110' />
-        </Link>
+    <div className="flex justify-center items-center h-screen bg-gray-200">
+      <div className="p-8 w-1/3 bg-white rounded-lg shadow-lg h-5/6">
+        <div className='flex justify-between items-center'>
+          <h1 className='text-3xl my-8 font-semibold text-black'>What's the plan for today?</h1>
+          <Link to='/tasks/create'>
+            <MdOutlineAddBox className='text-sky-800 text-4xl hover:scale-110' />
+          </Link>
+        </div>
+        {loading ? (
+          <Spinner />
+        ) : (
+          <ul className="divide-y divide-gray-300">
+            {tasks.map((task, index) => (
+               <li key={task._id} 
+               className={`task-item p-4 flex justify-between items-center border border-gray-200 rounded-lg shadow-md mt-1 
+               hover:cursor-pointer ${task.status === 'completed' ? 'bg-green-300' : ''}`}
+               >
+                <div className="flex items-center">
+                  <input
+                    type="checkbox"
+                    className="mr-2 h-5 w-5 accent-green-500 border-gray-300 rounded-md hover:cursor-pointer"
+                    onChange={() => handleCheckboxChange(task._id)}
+                    checked={task.status === 'completed'}
+                  />
+                  <span className="text-2xl font-bold mr-2">{index + 1}.</span>
+                  <span className="text-2xl">{task.title}</span>
+                </div>
+                <div className="flex gap-2">
+                  <Link to={`/tasks/details/${task._id}`} className="mr-2">
+                    <BsInfoCircle className="text-3xl text-gray-500 hover:text-gray-700 cursor-pointer" />
+                  </Link>
+                  <Link to={`/tasks/edit/${task._id}`} className="mr-2">
+                    <AiOutlineEdit className="text-3xl text-yellow-500 hover:text-yellow-600 cursor-pointer" />
+                  </Link>
+                  <Link to={`/tasks/delete/${task._id}`} className="mr-2">
+                    <MdOutlineDelete className="text-3xl text-red-500 hover:text-red-700 cursor-pointer" />
+                  </Link>
+                </div>
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
-      {loading ? (
-        <Spinner />
-      ) : (
-        <ul className="list-disc">
-          {tasks.map((task, index) => (
-            <li key={task.id} className="text-2xl my-2 flex justify-between items-center">
-              <div>
-                <span className="text-2xl font-bold mr-2">{index + 1}.</span>
-                {task.title}
-              </div>
-              <div className="flex gap-2">
-              <Link to={`/tasks/details/${task._id}`} className="mr-2">
-                  <BsInfoCircle className="text-3xl text-green-800 hover:scale-110 cursor-pointer" />
-                </Link>
-                <Link to={`/tasks/edit/${task._id}`} className="mr-2">
-                  <AiOutlineEdit className="text-3xl text-yellow-600 hover:scale-110 cursor-pointer" />
-                </Link>
-                <Link to={`/tasks/delete/${task._id}`} className="mr-2">
-                <MdOutlineDelete className="text-3xl text-red-600 hover:scale-110 cursor-pointer" />
-                </Link>
-              </div>
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
     </div>
   );
-};
-
-export default Home
+}
+export default Home;
