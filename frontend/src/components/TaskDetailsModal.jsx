dff17f1475baa6d940d220cc9a1d10936dc01975
valgeorg97 <valentingg1997@gmail.com>
2024-04-04 15:42:59 +0300

    Make TaskDetailsModal responsive

--- frontend/src/components/TaskDetailsModal.jsx
+++ frontend/src/components/TaskDetailsModal.jsx
@@ -15,34 +15,34 @@ const TaskDetailsModal = ({ task, onClose }) => {
     <div className="fixed bg-black bg-opacity-60 top-0 left-0 right-0 bottom-0 z-50 flex justify-center items-center">
       <div
         onClick={(event) => event.stopPropagation()}
-        className="w-[600px] max-w-full h-[500px] bg-white rounded-xl p-4 flex flex-col relative"
+        className="max-w-[90%] md:max-w-[600px] w-full md:h-[500px] bg-white rounded-xl p-4 flex flex-col relative"
       >
         <AiOutlineClose
           className="absolute right-6 top-6 text-3xl text-red-600 cursor-pointer"
           onClick={onClose}
         />
         <div className="flex justify-between items-center mb-4">
-          <h1 className="text-3xl">Task Details</h1>
+          <h1 className="lg:text-3xl md:text-2xl font-bold">Task Details</h1>
         </div>
         <div className="my-4">
           <span className="text-xl mr-4 text-gray-500">Title:</span>
-          <span className="text-2xl">{task.title}</span>
+          <span className="lg:text-2xl md:text-xl">{task.title}</span>
         </div>
         <div className="my-4">
           <span className="text-xl mr-4 text-gray-500">Description:</span>
-          <span className="text-2xl break-words">{task.description}</span>
+          <span className="lg:text-2xl md:text-xl break-words">{task.description}</span>
         </div>
         <div className="my-4">
           <span className="text-xl mr-4 text-gray-500">Status:</span>
-          <span className="text-2xl">{task.status}</span>
+          <span className="lg:text-2xl md:text-xl">{task.status}</span>
         </div>
         <div className="my-4">
           <span className="text-xl mr-4 text-gray-500">Create Time:</span>
-          <span className="text-2xl">{formatDate(task.createdAt)}</span>
+          <span className="lg:text-2xl md:text-xl">{formatDate(task.createdAt)}</span>
         </div>
         <div className="my-4">
           <span className="text-xl mr-4 text-gray-500">Last Update Time:</span>
-          <span className="text-2xl">{formatDate(task.updatedAt)}</span>
+          <span className="lg:text-2xl md:text-xl">{formatDate(task.updatedAt)}</span>
         </div>
       </div>
     </div>
